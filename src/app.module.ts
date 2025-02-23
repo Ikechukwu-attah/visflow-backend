d6c0e3951e6696cb48490dcfec968c647d47bdd5
Ikechukwu-attah <attahikechukwu@gmail.com>
2025-02-23 13:13:55 -0500

    services folder initialized

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { VisaRecommendationModule } from './visa-recommendation/visa-recommendation.module';
+import { JobRecommendationModule } from './job-recommendation/job-recommendation.module';
+import { DocumentGenerationModule } from './document-generation/document-generation.module';
+import { ConsultantMatchingModule } from './consultant-matching/consultant-matching.module';
+import { PaymentModule } from './payment/payment.module';
+import { ChatbotModule } from './chatbot/chatbot.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, VisaRecommendationModule, JobRecommendationModule, DocumentGenerationModule, ConsultantMatchingModule, PaymentModule, ChatbotModule],
   controllers: [AppController],
   providers: [AppService],
 })
